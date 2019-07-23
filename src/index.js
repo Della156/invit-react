c36c21b36cf6b21418e44f3b4441a87a48e67715
duanyuting <duanyuting@utry.cn>
2019-07-23 16:57:34 +0800

    feat: 引入ant mobile 并且实现按需加载

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/*
+入口JS
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/css/index.less";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import { Button } from "antd-mobile";
+ReactDOM.render(
+  <div>
+    <Button>Start</Button>
+    <p className="ce-shi">color</p>
+  </div>,
+  document.getElementById("root")
+);
