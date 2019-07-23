17a07eee3b2a516b31dcb5469966208be35bc1f0
duanyuting <duanyuting@utry.cn>
2019-07-23 15:00:20 +0800

    Updates

---
+++ precache-manifest.319503463ae713d1cc5fe05a38e72740.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86cd9235b1659599a34da63aa28a94fa",
+    "url": "/invit-react/index.html"
+  },
+  {
+    "revision": "3710a53e977d78049269",
+    "url": "/invit-react/static/css/main.5c016f5d.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/invit-react/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "3710a53e977d78049269",
+    "url": "/invit-react/static/js/main.22fa476e.chunk.js"
+  },
+  {
+    "revision": "be56943e833ed8324fef",
+    "url": "/invit-react/static/js/runtime~main.ba1937f4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/invit-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
